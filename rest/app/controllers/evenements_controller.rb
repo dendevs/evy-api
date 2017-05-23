b22415863bd62056258bab52e48f9c34ad751abe
dendev <ddv@awt.be>
2017-05-23 11:59:17 +0200

    1er resource evenement

---
+++ rest/app/controllers/evenements_controller.rb
@@ -0,0 +1,53 @@
+class EvenementsController < ApplicationController
+    skip_before_filter :verify_authenticity_token
+
+    def index
+        @evenements = Evenement.all
+    end 
+
+    def show
+        @evenement = Evenement.find( params[:id] )
+    end
+
+    def new
+        @evenement = Evenement.new
+    end
+
+    def edit
+        @evenement = Evenement.find( params[:id] )
+    end
+
+    def create
+        @evenement = Evenement.new( evenement_params )
+
+        if @evenement.save
+            redirect_to @evenement
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @evenement.find( params[:id] )
+
+        if @evenement.update( evenement_params )
+            redirect_to @evenement
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @evenement.finf( params[:id] )
+        @evenement.destroy
+
+        redirect_to evenements_path
+    end
+
+    private
+    def evenement_params
+        params.require(:evenement).permit( :title, :description )
+    end
+
+    # edit create update destroy
+end
---
+++ rest/app/models/evenement.rb
@@ -0,0 +1,4 @@
+class Evenement < ApplicationRecord
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 20 }
+end
